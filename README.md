16761a7f0321c82ff29a7dcff978974ca43692a8
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2026-01-12 23:31:53 -0500

    weight

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 A thing-selectoror.
 
 TO-DO
-Remove text select on buttons
+Remove text select on buttons (12/1/26)
 
-Make weight and active meaningful
+Make weight meaningful (integer weights) (12/1/26)
+
+Make active toggle correlate to an intWeight of 0.
+
+Alert modal
--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function addRow() {
   var active = init.insertCell(2);
 
   item.setAttribute('contentEditable', true);
+  weight.setAttribute('contentEditable', true);
 
   active.classList.add('toggle');
 
@@ -22,11 +23,34 @@ function addRow() {
 // table.tBodies[0].rows.length
 
 var decide = document.getElementById("toggle");
-decide.addEventListener("click", pickRow);
+decide.addEventListener("click", weightedPick);
 
 function pickRow() {
   var pick = Math.floor(Math.random() * table.tBodies[0].rows.length);
   console.log("Picked: " + table.tBodies[0].rows[pick].cells[0].innerHTML);
 
   alert("Picked: " + table.tBodies[0].rows[pick].cells[0].innerHTML);
+}
+
+function weightedPick(){
+
+  var weightSum = 0;
+  var weightChoice = 0;
+
+  for (var i = 0; i < table.tBodies[0].rows.length; i++){
+    weightSum += parseInt(table.tBodies[0].rows[i].cells[1].innerHTML, 10);
+    //console.log(weightSum);
+  }
+
+  var pick = Math.floor(Math.random() * weightSum);
+
+  console.log(pick);
+
+  while (pick >= parseInt(table.tBodies[0].rows[weightChoice].cells[1].innerHTML, 10)) {
+    pick -= parseInt(table.tBodies[0].rows[weightChoice].cells[1].innerHTML, 10);
+    weightChoice++;
+  }
+
+  console.log( table.tBodies[0].rows[weightChoice].cells[0].innerHTML)
+
 }
